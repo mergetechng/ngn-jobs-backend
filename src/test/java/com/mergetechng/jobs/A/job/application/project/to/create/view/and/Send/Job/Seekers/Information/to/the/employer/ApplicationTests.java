4d14c413c94cddfeff5fbf50c8c3fd243ff8014c
keemsisi <keemsisi@gmail.com>
2021-07-03 04:43:43 +0100

    initial commit

---
+++ src/test/java/com/mergetechng/jobs/A/job/application/project/to/create/view/and/Send/Job/Seekers/Information/to/the/employer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mergetechng.jobs.A.job.application.project.to.create.view.and.Send.Job.Seekers.Information.to.the.employer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
