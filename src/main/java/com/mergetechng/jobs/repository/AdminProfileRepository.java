cafe0630fdbbcef23970b1a2b1670509662388eb
keemsisi <keemsisi@gmail.com>
2021-07-13 13:26:43 +0100

    removed error

--- src/main/java/com/mergetechng/jobs/repository/AdminProfileRepository.java
+++ src/main/java/com/mergetechng/jobs/repository/AdminProfileRepository.java
@@ -2,7 +2,12 @@ package com.mergetechng.jobs.repository;
 
 
 import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
 
-@Repository
-public class AdminProfileRepository {
-}
+
+//@Repository
+//public interface CustomerRepository extends MongoRepository<UserDocument, String> {
+////    public Customer findByUsername(String firstName);
+////    public List<Customer> findByLastName(String lastName);
+//}
