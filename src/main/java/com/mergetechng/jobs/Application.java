30027d9c98de3f60c96b8e752aae721c28a086c3
keemsisi <keemsisi@gmail.com>
2021-07-30 22:50:38 +0100

    minor change

--- src/main/java/com/mergetechng/jobs/Application.java
+++ src/main/java/com/mergetechng/jobs/Application.java
@@ -1,10 +1,8 @@
 package com.mergetechng.jobs;
 
-import io.github.kaiso.relmongo.config.EnableRelMongo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
